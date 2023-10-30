456a53d66c99544173bd910d17d6a65fa541e9bc
Emma <emmaolssondev@outlook.com>
2023-10-30 09:56:19 +0100

    new base color

--- app/components/shared/loggan.tsx
+++ app/components/shared/loggan.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image'
 export default function Loggan() {
     return (
         <div className="flex flex-row items-center">
-        <Image src='/LCubelogo_transp_liten.gif' alt="Företags loggan, tre svarta L" width={80}
+        <Image className="log" src='/LCubelogo_transp_liten.gif' alt="Företags loggan, tre svarta L" width={80}
             height={80} />
-            <p className="font-bold text-4xl">LCube</p>
+            <p className="font-bold text-4xl navbar__item">LCube</p>
         </div>
     )
 }
